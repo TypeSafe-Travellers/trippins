89ebc1b08634c138c569ffb70d91d5d23cf6c838
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-06 17:32:21 +0530

    base setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,20 @@ import { api } from "../utils/api";
 
 import "../styles/globals.css";
 
+import localfont from "@next/font/local";
+const defaultFont = localfont({
+  src: "../fonts/FoundersGrotesk-Bold.woff2",
+});
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <main className={defaultFont.className}>
+        <Component {...pageProps} />
+      </main>
     </SessionProvider>
   );
 };
