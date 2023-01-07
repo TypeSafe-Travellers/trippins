6a8f437c712115ee929f1db80ab3e19b0acca810
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-07 15:42:12 +0530

    remove unused class

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
           <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-cyan-400 to-emerald-400 opacity-75 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
           <div className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-black px-7 py-4 leading-none">
             <p
-              className={`mx-5 bg-gradient-to-r from-cyan-600 to-emerald-600 bg-clip-text px-6 pt-5 text-center text-4xl font-light text-transparent lg:text-7xl ${boldFont.className}`}
+              className={`mx-5 bg-gradient-to-r from-cyan-600 to-emerald-600 bg-clip-text px-6 pt-5 text-center text-4xl text-transparent lg:text-7xl ${boldFont.className}`}
             >
               {hello.data ? hello.data.greeting : "Loading greeting..."}
             </p>
