d55d842675f8df4c3910998cb503bf7cce3bdc88
Ayanava Karmakar <karmakarayanava2021@gmail.com>
2023-01-07 13:23:00 +0000

    integrate replit

---
+++ replit.nix
@@ -0,0 +1,12 @@
+{ pkgs }: {
+    deps = [
+        pkgs.yarn
+        pkgs.esbuild
+        pkgs.nodejs-16_x
+
+        pkgs.nodePackages.typescript
+        pkgs.nodePackages.typescript-language-server
+      
+        pkgs.openssl
+    ];
+}
