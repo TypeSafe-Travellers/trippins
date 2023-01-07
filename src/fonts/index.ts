9ee0131cb6991be378dc1854f9a60e588b166349
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-07 15:44:31 +0530

    preload

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -3,24 +3,29 @@ import localfont from "@next/font/local";
 export const lightFont = localfont({
   src: "./FoundersGrotesk-Light.woff2",
   variable: "--font-GroteskLight",
+  preload: true,
 });
 
 export const regularFont = localfont({
   src: "./FoundersGrotesk-Regular.woff2",
   variable: "--font-GroteskRegular",
+  preload: true,
 });
 
 export const semiBoldFont = localfont({
   src: "./FoundersGrotesk-Semibold.woff2",
   variable: "--font-GroteskSemiBold",
+  preload: true,
 });
 
 export const boldFont = localfont({
   src: "./FoundersGrotesk-Bold.woff2",
   variable: "--font-GroteskBold",
+  preload: true,
 });
 
 export const condensedBoldFont = localfont({
   src: "./FoundersGroteskXCond-Bold.woff2",
   variable: "--font-GroteskXCondBold",
+  preload: true,
 });
