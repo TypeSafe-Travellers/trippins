c10c2621077e2fe8f60599e692a19239d9311d36
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-08 18:52:18 +0530

    fix import order

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
-import { ThemeProvider } from "next-themes";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
