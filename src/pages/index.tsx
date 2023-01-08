a5519434cf441c1d64ff0d74c78f3a8edb478806
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-08 18:57:45 +0530

    fix button position

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,21 +19,23 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex min-h-screen items-center justify-center dark:bg-black">
-        <Link
-          className="group relative mx-10 cursor-pointer"
-          href={"https://github.com/TypeSafe-Travellers/App"}
-          target={"_blank"}
-        >
-          <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-cyan-400 to-emerald-400 opacity-75 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-          <div className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-black px-7 py-4 leading-none">
-            <p
-              className={`mx-5 bg-gradient-to-r from-cyan-600 to-emerald-600 bg-clip-text px-6 pt-5 text-center text-4xl text-transparent lg:text-7xl ${boldFont.className}`}
-            >
-              {hello.data ? hello.data.greeting : "Loading greeting..."}
-            </p>
-          </div>
-        </Link>
-        <ThemeToggle />
+        <div className="flex flex-col items-center gap-10">
+          <Link
+            className="group relative mx-10 cursor-pointer"
+            href={"https://github.com/TypeSafe-Travellers/App"}
+            target={"_blank"}
+          >
+            <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-cyan-400 to-emerald-400 opacity-75 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
+            <div className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-black px-7 py-4 leading-none">
+              <p
+                className={`mx-5 bg-gradient-to-r from-cyan-600 to-emerald-600 bg-clip-text px-6 pt-5 text-center text-4xl text-transparent lg:text-7xl ${boldFont.className}`}
+              >
+                {hello.data ? hello.data.greeting : "Loading greeting..."}
+              </p>
+            </div>
+          </Link>
+          <ThemeToggle />
+        </div>
       </main>
     </>
   );
