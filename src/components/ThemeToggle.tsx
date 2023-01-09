2358ef9b4ccefbe66884a50fbff85c81045c0c15
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-09 14:34:52 +0530

    basic design

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from "next-themes";
 import { useState, useEffect } from "react";
 import * as Toggle from "@radix-ui/react-toggle";
+import * as Tooltip from "@radix-ui/react-tooltip";
 import { SunIcon, MoonIcon } from "../icons";
 
 export const ThemeToggle = () => {
@@ -18,20 +19,35 @@ export const ThemeToggle = () => {
   }
 
   return (
-    <Toggle.Root
-      defaultPressed={lightMode}
-      onPressedChange={setLightMode}
-      asChild
-    >
-      <button
-        onClick={() => {
-          setTheme(theme === "dark" ? "light" : "dark");
-        }}
-        className="2xl cursor-pointer text-black dark:text-white"
-        aria-label="Toggle light/dark mode"
-      >
-        {lightMode ? <MoonIcon /> : <SunIcon />}
-      </button>
-    </Toggle.Root>
+    <Tooltip.Provider>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <Toggle.Root
+            defaultPressed={lightMode}
+            onPressedChange={setLightMode}
+            asChild
+          >
+            <button
+              onClick={() => {
+                setTheme(theme === "dark" ? "light" : "dark");
+              }}
+              className="2xl cursor-pointer text-black dark:text-white"
+              aria-label="Toggle light/dark mode"
+            >
+              {lightMode ? <MoonIcon /> : <SunIcon />}
+            </button>
+          </Toggle.Root>
+        </Tooltip.Trigger>
+        <Tooltip.Content
+          sideOffset={4}
+          className="radix-side-top:animate-slide-down-fade radix-side-right:animate-slide-left-fade radix-side-bottom:animate-slide-up-fade radix-side-left:animate-slide-right-fade inline-flex items-center rounded-md bg-white px-4 py-2.5 dark:bg-gray-800"
+        >
+          <Tooltip.Arrow className="fill-current text-white dark:text-gray-800" />
+          <span className="block text-xs leading-none text-gray-700 dark:text-gray-100">
+            Toggle Theme
+          </span>
+        </Tooltip.Content>
+      </Tooltip.Root>
+    </Tooltip.Provider>
   );
 };
