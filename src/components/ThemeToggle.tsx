c93cbf0387dc5f56cd7d56fedbe735954ac81cd4
Ayanava Karmakar <karmakarayanava2021@gmail.com>
2023-01-12 00:46:41 +0530

    feat: design ThemeToggler using Radix UI (#31)

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,8 +1,15 @@
 import { useTheme } from "next-themes";
 import { useState, useEffect } from "react";
+import * as Toggle from "@radix-ui/react-toggle";
+import * as Tooltip from "@radix-ui/react-tooltip";
+import clsx from "clsx";
+import { regularFont } from "../fonts";
+import { SunIcon, MoonIcon } from "../icons";
+
 export const ThemeToggle = () => {
-  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const [lightMode, setLightMode] = useState(theme === "light" ? true : false);
 
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
@@ -14,15 +21,53 @@ export const ThemeToggle = () => {
   }
 
   return (
-    <button
-      id="theme-toggle"
-      type="button"
-      className="rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-      onClick={() => {
-        setTheme(theme === "dark" ? "light" : "dark");
-      }}
-    >
-      Change Theme
-    </button>
+    <Tooltip.Provider>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <Toggle.Root
+            defaultPressed={lightMode}
+            onPressedChange={setLightMode}
+            asChild
+          >
+            <button
+              onClick={() => {
+                setTheme(theme === "dark" ? "light" : "dark");
+              }}
+              className={clsx(
+                "cursor-pointer",
+                "text-black dark:text-gray-200",
+              )}
+              aria-label="Toggle light/dark mode"
+            >
+              {lightMode ? <MoonIcon /> : <SunIcon />}
+            </button>
+          </Toggle.Root>
+        </Tooltip.Trigger>
+        <Tooltip.Content
+          sideOffset={4}
+          className={clsx(
+            "radix-side-top:animate-slide-down-fade",
+            "radix-side-right:animate-slide-left-fade",
+            "radix-side-bottom:animate-slide-up-fade",
+            "radix-side-left:animate-slide-right-fade",
+            "bg-gray-900 dark:bg-gray-200",
+            "rounded-md px-4 pt-4 pb-2",
+            "inline-flex items-center",
+          )}
+        >
+          <Tooltip.Arrow
+            className={clsx("fill-current", "text-gray-900 dark:text-gray-200")}
+          />
+          <span
+            className={clsx(
+              "block text-lg text-gray-200 dark:text-gray-900",
+              `${regularFont.className}`,
+            )}
+          >
+            Toggle Theme
+          </span>
+        </Tooltip.Content>
+      </Tooltip.Root>
+    </Tooltip.Provider>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,17 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
-import { ThemeProvider } from "next-themes";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <ThemeProvider defaultTheme="dark">
+    <ThemeProvider defaultTheme="dark" attribute="class">
       <SessionProvider session={session}>
         <Component {...pageProps} />
       </SessionProvider>
