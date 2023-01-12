662a1da673b6a479c52947faf47aa7887f70a89f
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-13 02:39:52 +0530

    fix light

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -5,15 +5,22 @@ import { boldFont, semiBoldFont } from "../fonts";
 
 const Profile: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col justify-center gap-2">
-      <div className={clsx("text-center text-7xl", `${boldFont.className}`)}>
+    <div className={clsx("flex min-h-screen justify-center", "flex-col gap-2")}>
+      <div
+        className={clsx(
+          "text-center text-7xl",
+          "mx-auto",
+          `${boldFont.className}`,
+        )}
+      >
         Profile Details
       </div>
       <div
         className={clsx(
           "mx-auto px-8 pt-6 pb-4",
-          "bg-white dark:bg-black",
-          "shadow-lg shadow-red-500 hover:shadow-blue-500",
+          "bg-gradient-to-tl from-red-200/20 via-gray-100 to-blue-200/20",
+          "dark:bg-black",
+          "shadow-lg shadow-blue-200 hover:shadow-red-200",
           "cursor-pointer",
           "rounded-lg border-4 border-solid border-black dark:border-white",
         )}
