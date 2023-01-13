89571ee9e74fddf5cca931db7f84e060ca17a1bb
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-13 18:03:44 +0530

    better seo

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,21 +1,32 @@
 import clsx from "clsx";
 import { type NextPage } from "next";
+import Head from "next/head";
 import { ProfileContainer, ThemeToggle } from "../components";
 
 const Profile: NextPage = () => {
   return (
-    <div
-      className={clsx(
-        "flex min-h-screen justify-center",
-        "flex-col gap-2",
-        "text-gray-900 dark:text-zinc-200",
-      )}
-    >
-      <ProfileContainer />
-      <div className={clsx("absolute right-5 top-0 pt-5")}>
-        <ThemeToggle />
-      </div>
-    </div>
+    <>
+      <Head>
+        <title>TypeSafe Travellers | Profile</title>
+        <meta name="description" content="Group Trip Planning App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div
+          className={clsx(
+            "flex min-h-screen justify-center",
+            "flex-col gap-2",
+            "text-gray-900 dark:text-zinc-200",
+          )}
+        >
+          <ProfileContainer />
+          <div className={clsx("absolute right-5 top-0 pt-5")}>
+            <ThemeToggle />
+          </div>
+        </div>
+      </main>
+    </>
   );
 };
 
