8659fceb5e7b7cc655c7d0ba10eaffc00dc386a8
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-14 01:27:12 +0530

    fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,18 +8,18 @@ export const Navbar = () => {
     <NavigationMenu.Root
       className={clsx("relative", `${regularFont.className}`)}
     >
-      <NavigationMenu.List className="flex flex-row space-x-2 rounded-lg bg-white p-2 dark:bg-gray-800">
+      <NavigationMenu.List className="flex flex-row space-x-2 rounded-lg bg-white px-2 pt-3 pb-1 dark:bg-gray-800">
         <NavigationMenu.Item asChild>
           <NavigationMenu.Link
             href="/"
             className={clsx(
-              "rounded-md p-2 hover:bg-gray-100 dark:hover:bg-gray-900",
+              "rounded-md px-2 py-3 hover:bg-gray-100 dark:hover:bg-gray-900",
               "text-lg text-gray-700 dark:text-gray-100",
             )}
           >
             <NavigationMenu.Trigger
               className={clsx(
-                "rounded-md p-2 text-lg hover:bg-gray-100 dark:hover:bg-gray-900",
+                "rounded-md text-lg hover:bg-gray-100 dark:hover:bg-gray-900",
                 "text-gray-700 dark:text-gray-100",
                 "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75",
               )}
@@ -33,13 +33,13 @@ export const Navbar = () => {
           <NavigationMenu.Link
             href="/about"
             className={clsx(
-              "rounded-md p-2 hover:bg-gray-100 dark:hover:bg-gray-900",
+              "rounded-md px-2 py-3 hover:bg-gray-100 dark:hover:bg-gray-900",
               "text-gray-700 dark:text-gray-100",
             )}
           >
             <NavigationMenu.Trigger
               className={clsx(
-                "rounded-md p-2 hover:bg-gray-100 dark:hover:bg-gray-900",
+                "rounded-md hover:bg-gray-100 dark:hover:bg-gray-900",
                 "text-lg text-gray-700 dark:text-gray-100",
                 "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75",
               )}
@@ -53,13 +53,13 @@ export const Navbar = () => {
           <NavigationMenu.Link
             href="/about"
             className={clsx(
-              "rounded-md p-2 hover:bg-gray-100 dark:hover:bg-gray-900",
+              "rounded-md px-2 py-3 hover:bg-gray-100 dark:hover:bg-gray-900",
               "text-lg text-gray-700 dark:text-gray-100",
             )}
           >
             <NavigationMenu.Trigger
               className={clsx(
-                "rounded-md p-2 hover:bg-gray-100 dark:hover:bg-gray-900",
+                "rounded-md hover:bg-gray-100 dark:hover:bg-gray-900",
                 "text-lg text-gray-700 dark:text-gray-100",
                 "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75",
               )}
