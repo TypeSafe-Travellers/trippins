a7151bafd87413ff8d7e6ade356f4897c4851f8e
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-15 01:23:16 +0530

    run prettier

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -11,8 +11,8 @@ export const LoginButton = () => {
         "inline-flex items-center rounded-md text-xl",
         " bg-green-100 text-center text-black",
         "border-2 border-solid border-black",
-        "hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-black",
-        "dark:bg-green-700 dark:text-white dark:hover:bg-green-600 dark:focus:ring-gray-500",
+        "focus:outline-none focus:ring-2 focus:ring-black hover:bg-green-200",
+        "dark:bg-green-700 dark:text-white dark:focus:ring-gray-500 dark:hover:bg-green-600",
         `${regularFont.className}`,
       )}
       onClick={() => signIn()}
--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -12,8 +12,8 @@ export const LogoutButton = () => {
         "inline-flex items-center rounded-md text-base",
         "bg-red-50 text-center text-black",
         "border-2 border-solid border-black",
-        "hover:bg-red-100/75 focus:outline-none focus:ring-2 focus:ring-black",
-        "dark:bg-red-600/90 dark:text-white dark:hover:bg-red-600 dark:focus:ring-gray-500",
+        "focus:outline-none focus:ring-2 focus:ring-black hover:bg-red-100/75",
+        "dark:bg-red-600/90 dark:text-white dark:focus:ring-gray-500 dark:hover:bg-red-600",
         `${regularFont.className}`,
       )}
       onClick={() => signOut()}
