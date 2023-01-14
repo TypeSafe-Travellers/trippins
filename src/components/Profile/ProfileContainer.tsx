9ef656b0ac1b5f35984ae52185a0c35e44f74075
Ayanava Karmakar <karmakarayanava2021@gmail.com>
2023-01-14 23:14:30 +0530

    feat: add profile page (#49)

---
+++ src/components/Profile/ProfileContainer.tsx
@@ -0,0 +1,20 @@
+import clsx from "clsx";
+import { boldFont } from "../../fonts";
+import { ProfileDetails } from "./ProfileDetails";
+
+export const ProfileContainer = () => {
+  return (
+    <>
+      <div
+        className={clsx(
+          "text-center text-4xl lg:text-7xl",
+          "mx-auto",
+          `${boldFont.className}`,
+        )}
+      >
+        Profile Details
+      </div>
+      <ProfileDetails />
+    </>
+  );
+};
