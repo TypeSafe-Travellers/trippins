11fc93b8dd96798a5a4c2615f0daaf9c7ce092f2
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-14 21:24:49 +0530

    fix des

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -56,26 +56,38 @@ export const LogOutButton = () => {
               forceMount
               className={clsx(
                 "fixed z-50",
-                "w-[95vw] max-w-md rounded-lg p-4 md:w-full",
+                "w-[95vw] max-w-md rounded-xl p-4 md:w-full",
                 "top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]",
-                "bg-white dark:bg-gray-800",
-                "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75",
+                "bg-gray-200 dark:bg-gray-800",
+                "focus:outline-none focus-visible:ring focus-visible:ring-red-500 focus-visible:ring-opacity-75",
               )}
             >
-              <AlertDialog.Title className="text-sm font-medium text-gray-900 dark:text-gray-100">
+              <AlertDialog.Title
+                className={clsx(
+                  "px-2 pt-3",
+                  "text-2xl text-gray-900 dark:text-gray-100",
+                  `${regularFont.className}`,
+                )}
+              >
                 Are you absolutely sure?
               </AlertDialog.Title>
-              <AlertDialog.Description className="mt-2 text-sm font-normal text-gray-700 dark:text-gray-400">
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
+              <AlertDialog.Description
+                className={clsx(
+                  "mt-2 px-2 text-xl text-gray-700 dark:text-gray-400",
+                  `${regularFont.className}`,
+                )}
+              >
+                You will have to log in again if you want to access your
+                account.
               </AlertDialog.Description>
               <div className="mt-4 flex justify-end space-x-2">
                 <AlertDialog.Cancel
                   className={clsx(
-                    "inline-flex select-none justify-center rounded-md px-4 py-2 text-sm font-medium",
+                    "inline-flex select-none justify-center rounded-md px-4 pb-1 pt-2.5 text-lg",
+                    `${regularFont.className}`,
                     "bg-white text-gray-900 hover:bg-gray-50 dark:bg-gray-700 dark:text-gray-100 hover:dark:bg-gray-600",
                     "border border-gray-300 dark:border-transparent",
-                    "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75",
+                    "focus:outline-none focus-visible:ring focus-visible:ring-red-500 focus-visible:ring-opacity-75",
                   )}
                 >
                   Cancel
@@ -86,10 +98,11 @@ export const LogOutButton = () => {
                     setIsOpen(false);
                   }}
                   className={clsx(
-                    "inline-flex select-none justify-center rounded-md px-4 py-2 text-sm font-medium",
-                    "bg-purple-600 text-white hover:bg-purple-700 dark:bg-purple-700 dark:text-gray-100 dark:hover:bg-purple-600",
+                    `${regularFont.className}`,
+                    "inline-flex select-none justify-center rounded-md px-4 pb-1 pt-2.5 text-lg",
+                    "bg-red-600 text-white hover:bg-red-700 dark:bg-red-700 dark:text-gray-100 dark:hover:bg-red-600",
                     "border border-transparent",
-                    "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75",
+                    "focus:outline-none focus-visible:ring focus-visible:ring-red-500 focus-visible:ring-opacity-75",
                   )}
                 >
                   Confirm
