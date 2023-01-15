97dd3108aad0d1bbee63fe2e2292444019af0ebf
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-16 00:18:07 +0530

    add mutation for updating email

--- src/server/api/routers/userProfile.ts
+++ src/server/api/routers/userProfile.ts
@@ -6,7 +6,7 @@ export const userProfileRouter = createTRPCRouter({
   /**
    * updates the name of the user
    * requires the user's current email and the new name
-   * @param currentEmail - the user's current email
+   * @param userId - the user's current session id
    * @param newName - the new name of the user
    */
   updateName: protectedProcedure
@@ -24,6 +24,34 @@ export const userProfileRouter = createTRPCRouter({
           },
           data: {
             name: input.newName,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+
+  /**
+   * updates the name of the user
+   * requires the user's current email and the new name
+   * @param userId - the user's current session id
+   * @param newEmail - the new email of the user
+   */
+  updateEmail: protectedProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+        newEmail: z.string().email(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.user.update({
+          where: {
+            id: input.userId,
+          },
+          data: {
+            name: input.newEmail,
           },
         });
       } catch (error) {
