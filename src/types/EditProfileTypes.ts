3655ea7079804c23793e208def42cbd91c8d9d32
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-16 00:21:25 +0530

    add type for function

--- src/types/EditProfileTypes.ts
+++ src/types/EditProfileTypes.ts
@@ -7,4 +7,12 @@ const updateUserNameSchema = z.object({
   newName: z.string().min(1).max(50),
 });
 
+/**
+ * Edit Profile Email Types
+ */
+const updateUserEmailSchema = z.object({
+  newEmail: z.string().email(),
+});
+
 export type UpdateUserNameType = z.infer<typeof updateUserNameSchema>;
+export type UpdateUserEmailType = z.infer<typeof updateUserEmailSchema>;
