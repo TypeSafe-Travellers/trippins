20870b98246889c4f02e9d50634946bb5bf840d4
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-16 00:32:47 +0530

    fixes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,30 +1,53 @@
 import clsx from "clsx";
-import { boldFont } from "../fonts";
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Navbar } from "../components";
+import { regularFont, semiBoldFont } from "../fonts";
 
-const about = () => {
+const About: NextPage = () => {
   return (
-    <main
-      className={clsx(
-        "flex h-[calc(100vh-100px)] w-screen items-center justify-center",
-      )}
-    >
-      <h1 className={clsx("")}>Typesafe Travellers</h1>
-      <p className={clsx("")}>
-        This is a website for people who want to travel the world, but do not
-        want to be ripped off by the travel industry. Many a times, when a group
-        of friends want to travel together, they have to go through the hassle
-        of finding a travel agent, who will then book the tickets, hotels, and
-        other things for them. This is a very time consuming process, and the
-        travel agent will charge a hefty commission for this. This website aims
-        to solve this problem by providing a platform for people to book their
-        own tickets, hotels, and other things, and then share the cost with
-        their friends. This way, they can save a lot of money, and also have a
-        lot of fun.
-      </p>
-      <p>Made with &hearts; using the T3 stack</p>
-      <footer>&copy; 2023 Typesafe Travellers</footer>
-    </main>
+    <>
+      <Head>
+        <title>TypeSafe Travellers</title>
+        <meta name="description" content="Group Trip Planning App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <main
+        className={clsx(
+          "mx-12 my-auto",
+          "flex flex-col gap-2",
+          "h-[calc(100vh-70px)]",
+          "items-center justify-center text-center",
+        )}
+      >
+        <div className={clsx(`${semiBoldFont.className}`, "text-7xl")}>
+          Typesafe Travellers
+        </div>
+
+        <div className={clsx("text-2xl", `${regularFont.className}`)}>
+          This is a website for people who want to travel the world, but do not
+          want to be ripped off by the travel industry. Many a times, when a
+          group of friends want to travel together, they have to go through the
+          hassle of finding a travel agent, who will then book the tickets,
+          hotels, and other things for them. This is a very time consuming
+          process, and the travel agent will charge a hefty commission for this.
+          This website aims to solve this problem by providing a platform for
+          people to book their own tickets, hotels, and other things, and then
+          share the cost with their friends. This way, they can save a lot of
+          money, and also have a lot of fun.
+        </div>
+
+        <div className={clsx("text-2xl", `${regularFont.className}`)}>
+          Made with &hearts; using the T3 stack
+        </div>
+
+        <footer className={clsx(`${regularFont.className}`, "text-xl")}>
+          &copy; 2023 Typesafe Travellers
+        </footer>
+      </main>
+    </>
   );
 };
 
-export default about;
+export default About;
