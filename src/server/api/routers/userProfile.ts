5853b6509b0ca39232c9e37029282acd8d4d3324
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-15 21:12:54 +0530

    add router

--- src/server/api/routers/userProfile.ts
+++ src/server/api/routers/userProfile.ts
@@ -1,3 +1,33 @@
-import { createTRPCRouter } from "../trpc";
+import { z } from "zod";
 
-export const userProfileRouter = createTRPCRouter({});
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const userProfileRouter = createTRPCRouter({
+  /**
+   * updates the name of the user
+   * requires the user's current email and the new name
+   * @param currentEmail - the user's current email
+   * @param newName - the new name of the user
+   */
+  updateName: protectedProcedure
+    .input(
+      z.object({
+        currentEmail: z.string().email(),
+        newName: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.user.update({
+          where: {
+            email: input.currentEmail,
+          },
+          data: {
+            name: input.newName,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+});
