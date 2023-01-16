dc6758a574a0bf22793aadadb53474d44504cd24
Nishith Savla <nishithsavla005@gmail.com>
2023-01-16 18:02:17 +0530

    refactor: Remove redundant `text-white` class

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,11 +7,11 @@ const NotFound: NextPage = () => {
       <div className="flex min-h-screen items-center justify-center text-center">
         <div className="flex flex-col items-center">
           <div className="max-w-md px-4 sm:max-w-2xl sm:px-6 md:max-w-3xl lg:max-w-4xl lg:px-0 xl:max-w-5xl 2xl:max-w-6xl">
-            <h1 className="text-center text-4xl font-bold tracking-tight text-white sm:text-6xl sm:tracking-tight lg:text-[4rem] xl:text-[6rem] xl:tracking-tight 2xl:text-[6.5rem]">
+            <h1 className="text-center text-4xl font-bold tracking-tight sm:text-6xl sm:tracking-tight lg:text-[4rem] xl:text-[6rem] xl:tracking-tight 2xl:text-[6.5rem]">
               Page not found...
               <span className="text-[hsl(200,100%,60%)]"> 404!</span>
             </h1>
-            <div className="group mt-4 flex w-full items-center justify-center gap-4 text-white xl:mt-8">
+            <div className="group mt-4 flex w-full items-center justify-center gap-4 xl:mt-8">
               <Link href="/" className="text-xl">
                 Return to the homepage
                 <svg
