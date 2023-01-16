f624d92707095a30214f758c734e4f61718f1a74
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-16 19:15:59 +0530

    base des

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,40 +1,22 @@
-import Link from "next/link";
 import type { NextPage } from "next";
+import clsx from "clsx";
+import { boldFont } from "../fonts";
 
 const NotFound: NextPage = () => {
   return (
-    <main>
-      <div className="flex min-h-screen items-center justify-center text-center">
-        <div className="flex flex-col items-center">
-          <div className="max-w-md px-4 sm:max-w-2xl sm:px-6 md:max-w-3xl lg:max-w-4xl lg:px-0 xl:max-w-5xl 2xl:max-w-6xl">
-            <h1 className="text-center text-4xl font-bold tracking-tight sm:text-6xl sm:tracking-tight lg:text-[4rem] xl:text-[6rem] xl:tracking-tight 2xl:text-[6.5rem]">
-              Page not found...
-              <span className="text-[hsl(200,100%,60%)]"> 404!</span>
-            </h1>
-            <div className="group mt-4 flex w-full items-center justify-center gap-4 xl:mt-8">
-              <Link href="/" className="text-xl">
-                Return to the homepage
-                <svg
-                  className="stroke ml-2 -mr-1 inline h-3 stroke-current stroke-2"
-                  fill="none"
-                  viewBox="0 0 10 10"
-                  aria-hidden="true"
-                >
-                  <path
-                    className="opacity-0 transition group-hover:opacity-100"
-                    d="M0 5h7"
-                  ></path>
-                  <path
-                    className="transition group-hover:translate-x-[3px]"
-                    d="M1 1l4 4-4 4"
-                  ></path>
-                </svg>
-              </Link>
-            </div>
-          </div>
+    <div
+      className={clsx(
+        "flex min-h-screen",
+        "items-center justify-center text-center",
+      )}
+    >
+      <div className={clsx("flex flex-col", `${boldFont.className}`)}>
+        <div className={clsx("text-9xl")}>
+          Page not found...
+          <span className="text-sky-500"> 404!</span>
         </div>
       </div>
-    </main>
+    </div>
   );
 };
 
