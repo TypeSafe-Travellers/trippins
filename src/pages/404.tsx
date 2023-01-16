7cae03c3106c6d662fc36d3cb0b81913a04d979f
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-17 01:18:21 +0530

    revert

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import clsx from "clsx";
 import { boldFont } from "../fonts";
-import { Footer, HomePageButton } from "../components";
+import { HomePageButton } from "../components";
 
 const NotFound: NextPage = () => {
   return (
@@ -17,9 +17,6 @@ const NotFound: NextPage = () => {
           <span className="text-blue-600 dark:text-sky-500"> 404!</span>
         </div>
         <HomePageButton />
-        <div className={clsx("relative")}>
-          <Footer />
-        </div>
       </div>
     </div>
   );
