f28107eb22a484e1fd1b71023ea9e5798fde05d7
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-22 19:01:00 +0530

    add anim to trips

--- src/components/Home/Authenticated/TripsContainer.tsx
+++ src/components/Home/Authenticated/TripsContainer.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import { semiBoldFont } from "../../../fonts";
+import { motion } from "framer-motion";
 
 // TODO remove after intergrating backend
 const dummyTripsData = [
@@ -35,28 +36,56 @@ export const TripsContainer = () => {
   return (
     <div className={clsx("flex flex-col gap-10", "p-10")}>
       {dummyTripsData.map((trip) => (
-        <div
+        <motion.div
           key={trip.id}
-          className={clsx(
-            "text-2xl lg:text-3xl",
-            "mx-0 break-words px-3 pt-6 pb-4 lg:mx-20 lg:px-8",
-            "dark:bg-gradient-to-tl dark:from-black/70 dark:via-black/60 dark:to-black/50",
-            "shadow-lg shadow-blue-200 hover:shadow-red-200 dark:shadow-indigo-900 dark:hover:shadow-indigo-700",
-            "rounded-lg border-4 border-solid border-black dark:border-gray-200",
-            "cursor-pointer",
-          )}
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          whileHover={{ scale: 1.05 }}
+          transition={{
+            type: "spring",
+            stiffness: 300,
+            damping: 30,
+          }}
         >
           <div
-            className={clsx("my-2 leading-none", `${semiBoldFont.className}`)}
+            key={trip.id}
+            className={clsx(
+              "text-2xl lg:text-3xl",
+              "mx-0 break-words px-3 pt-6 pb-4 lg:mx-20 lg:px-8",
+              "dark:bg-gradient-to-tl dark:from-black/70 dark:via-black/60 dark:to-black/50",
+              "shadow-lg shadow-blue-200 hover:shadow-red-200 dark:shadow-indigo-900 dark:hover:shadow-indigo-700",
+              "rounded-lg border-4 border-solid border-black dark:border-gray-200",
+              "cursor-pointer",
+            )}
           >
-            {trip.name}
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 300,
+                damping: 30,
+              }}
+            >
+              <div
+                className={clsx(
+                  "my-2 leading-none",
+                  `${semiBoldFont.className}`,
+                )}
+              >
+                {trip.name}
+              </div>
+              <div
+                className={clsx(
+                  "my-2 leading-none",
+                  `${semiBoldFont.className}`,
+                )}
+              >
+                {trip.description}
+              </div>
+            </motion.div>
           </div>
-          <div
-            className={clsx("my-2 leading-none", `${semiBoldFont.className}`)}
-          >
-            {trip.description}
-          </div>
-        </div>
+        </motion.div>
       ))}
     </div>
   );
