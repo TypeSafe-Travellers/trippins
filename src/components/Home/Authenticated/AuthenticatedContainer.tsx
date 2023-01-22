dbd362f76d6d46b73f82c2427722bc22845c1aa4
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-22 19:04:21 +0530

    add footer

--- src/components/Home/Authenticated/AuthenticatedContainer.tsx
+++ src/components/Home/Authenticated/AuthenticatedContainer.tsx
@@ -1,12 +1,19 @@
+import { Footer } from "../../Misc/Footer";
 import { TripNav } from "./TripNav";
 import { TripsContainer } from "./TripsContainer";
 import clsx from "clsx";
 
 export const AuthenticatedContainer = () => {
   return (
-    <div className={clsx("p-5")}>
-      <TripNav />
-      <TripsContainer />
-    </div>
+    <>
+      <div className={clsx("p-5")}>
+        <TripNav />
+        <TripsContainer />
+      </div>
+
+      <div className={clsx("relative bottom-0 text-center lg:bottom-3")}>
+        <Footer />
+      </div>
+    </>
   );
 };
