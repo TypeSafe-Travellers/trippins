5eb51495c7f55ccf362fcf12237a5dd53a08b7e7
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-22 18:49:55 +0530

    add anim to 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import clsx from "clsx";
 import { boldFont } from "../fonts";
 import { HomePageButton } from "../components";
+import { motion } from "framer-motion";
 
 const NotFound: NextPage = () => {
   return (
@@ -11,13 +12,23 @@ const NotFound: NextPage = () => {
         "items-center justify-center text-center",
       )}
     >
-      <div className={clsx("flex flex-col", `${boldFont.className}`)}>
-        <div className={clsx("text-4xl lg:text-9xl")}>
-          Page not found...
-          <span className="text-blue-600 dark:text-sky-500"> 404!</span>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 300,
+          damping: 30,
+        }}
+      >
+        <div className={clsx("flex flex-col", `${boldFont.className}`)}>
+          <div className={clsx("text-4xl lg:text-9xl")}>
+            Page not found...
+            <span className="text-blue-600 dark:text-sky-500"> 404!</span>
+          </div>
+          <HomePageButton />
         </div>
-        <HomePageButton />
-      </div>
+      </motion.div>
     </div>
   );
 };
