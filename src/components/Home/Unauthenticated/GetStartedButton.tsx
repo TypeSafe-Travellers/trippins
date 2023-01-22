07f576387634c7ded2ae18879d2360f46709f3b7
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-22 17:50:41 +0530

    add animation for get started button

--- src/components/Home/Unauthenticated/GetStartedButton.tsx
+++ src/components/Home/Unauthenticated/GetStartedButton.tsx
@@ -1,23 +1,34 @@
 import clsx from "clsx";
 import { regularFont } from "../../../fonts";
 import { signIn } from "next-auth/react";
+import { motion } from "framer-motion";
 
 export const GetStartedButton = () => {
   return (
-    <button
-      className={clsx(
-        `${regularFont.className}`,
-        "inline-flex select-none items-center justify-center rounded-md px-5 pt-4 pb-2",
-        "mx-auto",
-        "text-2xl",
-        "shadow-lg shadow-blue-200 hover:shadow-red-200 dark:shadow-indigo-900 hover:dark:shadow-indigo-700",
-        "rounded-md border-2 border-solid border-black dark:border-gray-200",
-        "bg-white dark:bg-black",
-        "focus:outline-none focus-visible:ring focus-visible:ring-black focus-visible:ring-opacity-75",
-      )}
-      onClick={() => signIn(undefined, { callbackUrl: "/" })}
+    <motion.div
+      initial={{ y: 100, scale: 0 }}
+      animate={{ y: 0, scale: 1 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 25,
+      }}
     >
-      Get Started
-    </button>
+      <button
+        className={clsx(
+          `${regularFont.className}`,
+          "inline-flex select-none items-center justify-center rounded-md px-5 pt-4 pb-2",
+          "mx-auto",
+          "text-2xl",
+          "shadow-lg shadow-blue-200 hover:shadow-red-200 dark:shadow-indigo-900 hover:dark:shadow-indigo-700",
+          "rounded-md border-2 border-solid border-black dark:border-gray-200",
+          "bg-white dark:bg-black",
+          "focus:outline-none focus-visible:ring focus-visible:ring-black focus-visible:ring-opacity-75",
+        )}
+        onClick={() => signIn(undefined, { callbackUrl: "/" })}
+      >
+        Get Started
+      </button>
+    </motion.div>
   );
 };
