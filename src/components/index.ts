dbe662966b0bf81f9b2e7dd6d89cfaa02c003e7d
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-23 15:27:04 +0530

    refactor misc

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,10 +4,12 @@ export { HomeContainer } from "./Home/HomeContainer";
 // Profile
 export { ProfileContainer } from "./Profile/ProfileContainer";
 
-// General
-export { Footer } from "./Misc/Footer";
-export { HomePageButton } from "./Misc/HomePageButton";
-export { LogoutAndAlertButton } from "./Misc/LogoutAndAlertButton";
-export { LoginButton } from "./Misc/LoginButton";
-export { ThemeToggle } from "./Misc/ThemeToggle";
-export { Navbar } from "./Misc/Navbar";
+// Misc
+export {
+  Navbar,
+  Footer,
+  HomePageButton,
+  LogoutAndAlertButton,
+  LoginButton,
+  ThemeToggle,
+} from "./Misc";
