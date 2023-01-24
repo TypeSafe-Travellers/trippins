9381bad7a8af04c07e5a14508b0f4f0fc28ac2e4
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-24 18:12:50 +0530

    add mutation for new trips

--- src/server/api/routers/userTrips.ts
+++ src/server/api/routers/userTrips.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const userTripsRouter = createTRPCRouter({
@@ -25,4 +27,43 @@ export const userTripsRouter = createTRPCRouter({
       console.error("error", error);
     }
   }),
+
+  /**
+   * mutation to create a new trip
+   * @param name - name of the trip
+   * @param description - description of the trip
+   * @param startDate - start date of the trip
+   * @param endDate - end date of the trip
+   * @param adminId - id of the user who created the trip
+   * @param participants - id of users who are participating in the trip
+   */
+  createTrip: protectedProcedure
+    .input(
+      z.object({
+        name: z.string().min(3).max(10),
+        description: z.string().min(3).max(100),
+        startDate: z.date(),
+        endDate: z.date(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.trip.create({
+          data: {
+            name: input.name,
+            description: input.description,
+            startDate: input.startDate,
+            endDate: input.endDate,
+            adminId: ctx.session.user.id,
+            participants: {
+              connect: {
+                id: ctx.session.user.id,
+              },
+            },
+          },
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }),
 });
