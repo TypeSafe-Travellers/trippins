f08b20a48479b881020e72e3cea484959ae379f0
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-01-25 15:34:40 +0530

    hmm

--- src/components/Home/Authenticated/CopyTripIdButton.tsx
+++ src/components/Home/Authenticated/CopyTripIdButton.tsx
@@ -1,4 +1,4 @@
-import { type FC } from "react";
+import { useState, type FC } from "react";
 import clsx from "clsx";
 import { regularFont } from "../../../fonts";
 import { motion } from "framer-motion";
@@ -10,6 +10,9 @@ interface Props {
 }
 
 export const CopyTripIdButton: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const isMd = useMediaQuery("(min-width: 768px)");
+
   const handleCopyToClipboard = async (): Promise<void> => {
     try {
       await navigator.clipboard.writeText(props.tripId);
@@ -17,6 +20,19 @@ export const CopyTripIdButton: FC<Props> = (props) => {
       // TODO add toast
     } catch (err) {
       console.error("Failed to copy text: ", err);
+    }
+  };
+
+  const handleClick = (): void => {
+    handleCopyToClipboard();
+
+    if (open) {
+      setOpen(false);
+      setTimeout(() => {
+        setOpen(true);
+      }, 400);
+    } else {
+      setOpen(true);
     }
   };
 
@@ -30,23 +46,25 @@ export const CopyTripIdButton: FC<Props> = (props) => {
         damping: 25,
       }}
     >
-      <button
-        className={clsx(
-          `${regularFont.className}`,
-          "inline-flex select-none items-center justify-center rounded-md",
-          "lg:px-3 lg:pt-2.5 lg:pb-1",
-          "px-2 pt-2 pb-0.5",
-          "mx-auto",
-          "text-lg lg:text-xl",
-          "shadow-md shadow-blue-200 hover:shadow-red-200 dark:shadow-indigo-900 hover:dark:shadow-indigo-700",
-          "rounded-md border-2 border-solid border-black dark:border-gray-200",
-          "bg-white dark:bg-black",
-          "focus:outline-none focus-visible:ring focus-visible:ring-black focus-visible:ring-opacity-75",
-        )}
-        onClick={handleCopyToClipboard}
-      >
-        Share Trip
-      </button>
+      <Toast.Provider swipeDirection={isMd ? "right" : "down"}>
+        <button
+          className={clsx(
+            `${regularFont.className}`,
+            "inline-flex select-none items-center justify-center rounded-md",
+            "lg:px-3 lg:pt-2.5 lg:pb-1",
+            "px-2 pt-2 pb-0.5",
+            "mx-auto",
+            "text-lg lg:text-xl",
+            "shadow-md shadow-blue-200 hover:shadow-red-200 dark:shadow-indigo-900 hover:dark:shadow-indigo-700",
+            "rounded-md border-2 border-solid border-black dark:border-gray-200",
+            "bg-white dark:bg-black",
+            "focus:outline-none focus-visible:ring focus-visible:ring-black focus-visible:ring-opacity-75",
+          )}
+          onClick={handleClick}
+        >
+          Share Trip
+        </button>
+      </Toast.Provider>
     </motion.div>
   );
 };
