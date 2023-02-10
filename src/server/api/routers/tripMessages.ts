e705c22563db57db30c2cb274be0a61a2e563026
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-02-11 01:13:43 +0530

    procedure to get all messages

--- src/server/api/routers/tripMessages.ts
+++ src/server/api/routers/tripMessages.ts
@@ -2,4 +2,37 @@ import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
-export const tripMessagesRouter = createTRPCRouter({});
+export const tripMessagesRouter = createTRPCRouter({
+  /**
+   * query to get the messages for a trip
+   * @param tripId - the id of the trip
+   * @returns array of messages
+   */
+  getMessages: protectedProcedure
+    .input(z.object({ tripId: z.string().length(25) }))
+    .query(async ({ ctx, input }) => {
+      try {
+        return await ctx.prisma.message.findMany({
+          where: {
+            tripId: input.tripId,
+          },
+          select: {
+            id: true,
+            text: true,
+            createdAt: true,
+            senderId: true,
+            sender: {
+              select: {
+                name: true,
+              },
+            },
+          },
+          orderBy: {
+            createdAt: "asc",
+          },
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }),
+});
