2d421f24d631990b0ad6989419a4508cf84ab2f7
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-02-11 14:33:31 +0530

    move

--- src/components/Trip/TripChat.tsx
+++ src/components/Trip/TripChat.tsx
@@ -1,6 +1,6 @@
 export const TripChat = () => {
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col items-center justify-center gap-3 text-center">
       <label>send message</label>
       <input />
       <button type="button">send</button>
