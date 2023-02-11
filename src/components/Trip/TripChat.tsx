1e94f56fdef836b3e53105c196435dbf815f3f0b
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-02-12 00:55:49 +0530

    stuff

--- src/components/Trip/TripChat.tsx
+++ src/components/Trip/TripChat.tsx
@@ -46,8 +46,9 @@ export const TripChat: FC<Props> = (props) => {
       <div
         className={clsx(
           `${regularFont.className}`,
-          "mx-5 h-80 w-96 overflow-y-auto py-5 px-3",
           "flex flex-col gap-3 text-left",
+          "mx-5 h-80 w-96 overflow-y-auto py-5 px-3",
+          "scroll-smooth scrollbar-thin scrollbar-track-transparent scrollbar-thumb-zinc-500 dark:scrollbar-thumb-indigo-900",
           "rounded-lg border-4 border-solid border-black dark:border-gray-200",
           "shadow-lg shadow-blue-200 hover:shadow-red-200 dark:shadow-indigo-900 dark:hover:shadow-indigo-700",
           "bg-gradient-to-tl from-white/70 via-white/60 to-white/50 dark:from-black/70 dark:via-black/60 dark:to-black/50",
